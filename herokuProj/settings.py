d064339d3a92449f06d4c45b36731931974bf7e9
MDM DIZON <mdmd@cepalco.com.ph>
2020-10-04 00:52:07 +0800

    settingsdebugtrue

--- herokuProj/settings.py
+++ herokuProj/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a)ea4r4zepn9ajy10pqva19w3gy(3)^&dk8#qa(x34u!sfnt+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 # ALLOWED_HOSTS = ['your_domain.herokuapp.com']
 ALLOWED_HOSTS = ['*']
