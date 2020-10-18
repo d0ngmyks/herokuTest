e12a549be712f78f1fa7245b37ae111397236748
MDM DIZON <mdmd@cepalco.com.ph>
2020-10-18 18:40:44 +0800

    collectstatic

--- herokuProj/settings.py
+++ herokuProj/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
+        # 'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     # '/var/www/static/',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 STATIC_ROOT = 'staticfiles'
