d47c59e91a6e448b5ac243bc4163433cf27a82bc
MDM DIZON <mdmd@cepalco.com.ph>
2020-10-18 19:04:15 +0800

    staticfilesdel

--- herokuProj/settings.py
+++ herokuProj/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
+# STATICFILES_DIRS = [
+    # BASE_DIR / "static",
     # os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
-]
+# ]
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 STATIC_ROOT = 'staticfiles'
