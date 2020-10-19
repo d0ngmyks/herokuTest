170563a7ed6b8835e59f7315fac92ca8ba16a7b8
MDM DIZON <mdmd@cepalco.com.ph>
2020-10-19 15:36:52 +0800

    'elephant'

--- herokuProj/settings.py
+++ herokuProj/settings.py
@@ -124,12 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-    # BASE_DIR / "static",
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
     # os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
-# ]
+]
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 STATIC_ROOT = 'staticfiles'
+
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media-url/'
--- herokuProj/views.py
+++ herokuProj/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 
 class HomepageTemplateView(TemplateView):
-    template_name = 'homepage.html'
+    # template_name = 'homepage.html'
+    template_name = 'elephant_base.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
