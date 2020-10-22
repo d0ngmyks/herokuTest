cb53bb3efbc008ddf47bbd9fb6958fd4ddd70bb8
MDM DIZON <mdmd@cepalco.com.ph>
2020-10-22 11:51:31 +0800

    'vehicles'

--- vehicles/views.py
+++ vehicles/views.py
@@ -7,7 +7,6 @@ class VehiclesListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        breakpoint()
         import dash_bootstrap_components as dbc
 
         button = dbc.Button("Block button", color="primary", block=True)
